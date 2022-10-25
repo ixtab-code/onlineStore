36db9dad878cf169a07595643ff9532973e52fb9
ixtab <ibana4ik@gmail.com>
2022-10-25 10:05:01 +0300

    add db connection

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
+require("dotenv").config();
+
 const express = require('express');
 const app = express();
+
+const mongoose = require("mongoose");
+
+const MONGODB_URL = `mongodb+srv://ixtab:1@cluster0.jq65ufq.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose
+	.connect(MONGODB_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+    .then(() => console.log('DBConnection Successfull'))
+	.then(() => {
+		app.listen(process.env.PORT || 3000);
+		console.log(`Started on port ${process.env.PORT}`);
+	})
+	.catch((err) => console.log(err));
