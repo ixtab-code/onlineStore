6482e2b845758c45cb262d7cce4fc1be825e8aab
ixtab <ibana4ik@gmail.com>
2022-10-25 11:19:54 +0300

    add path util

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 
+const bodyParser = require("body-parser");
+
 const mongoose = require("mongoose");
 
 const MONGODB_URL = `mongodb+srv://ixtab:1@cluster0.jq65ufq.mongodb.net/?retryWrites=true&w=majority`;
