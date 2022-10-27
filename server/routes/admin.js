9c1c709d04f654a4fd6395c9e74dea71535d75f4
ixtab <ibana4ik@gmail.com>
2022-10-27 06:40:17 +0300

    add admin controller

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,14 +4,26 @@ const express = require('express');
 
 const rootDir = require('../utils/path');
 
-const productController = require("../controller/product");
+const adminController = require('../controller/admin');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get("/add-product", productController.getAddProduct);
+router.get("/add-product", adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products');
 
 // /admin/add-product => POST
-router.post("/add-product", productController.postAddProduct);
+router.post("/add-product", adminController.postAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
--- server/routes/shop.js
+++ server/routes/shop.js
@@ -7,6 +7,13 @@ const rootDir = require('../utils/path');
 const router = express.Router();
 
 const productController = require("../controller/product");
+
 router.get("/", productController.getProducts);
+
+router.get("/products", productController.getProducts);
+
+router.get("/cart");
+
+router.get("/checkout");
 
 module.exports = router;
